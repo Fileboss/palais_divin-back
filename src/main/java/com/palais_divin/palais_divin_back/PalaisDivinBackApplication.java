88ce61bf8fe2cfd74ab2ff1bf251acd9bedef58a
Fileboss <bausphilippe@gmail.com>
2025-03-17 18:00:22 +0100

    Project initialization
    Project initialization done with Spring initializr

---
+++ src/main/java/com/palais_divin/palais_divin_back/PalaisDivinBackApplication.java
@@ -0,0 +1,13 @@
+package com.palais_divin.palais_divin_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalaisDivinBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalaisDivinBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/palais_divin/palais_divin_back/PalaisDivinBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palais_divin.palais_divin_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalaisDivinBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
